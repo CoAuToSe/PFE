e6cae7219a5d7cdbab957912318b439666bc77a4
CATS <Aurelien.Couthures@gmail.com>
2025-09-18 09:31:27 +0200

    Create tello_launch_services.py

---
+++ Simulation_Gazebo_new/tello_ros_ws/src/tello_ros/tello_gazebo/launch/tello_launch_services.py
@@ -0,0 +1,44 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+
+def generate_launch_description():
+    namespace = 'tello_7'
+    world_path = os.path.join(get_package_share_directory('tello_gazebo'), 'worlds', 'simple.world')
+    urdf_path = os.path.join(get_package_share_directory('tello_description'), 'urdf', namespace + '.urdf')
+    return LaunchDescription([
+        # # Launch Gazebo, loading simple.world
+        # ExecuteProcess(cmd=[
+        #     'gazebo',
+        #     '--verbose',
+        #     '-s', 'libgazebo_ros_init.so',  # Publish /clock
+        #     '-s', 'libgazebo_ros_factory.so',  # Provide gazebo_ros::Node
+        #     world_path
+        # ], output='screen'),
+
+        # Publish static transforms
+        Node(package='robot_state_publisher', executable='robot_state_publisher', output='screen', namespace=namespace,
+             arguments=[urdf_path]),
+
+        # Joystick driver, generates /namespace/joy messages
+        # Node(package='joy', executable='joy_node', output='screen', namespace=namespace),
+
+        # Joystick controller, generates /namespace/cmd_vel messages
+        # Node(package='tello_driver', executable='tello_joy_main', output='screen', namespace=namespace),
+
+        Node(package='tello_driver', executable='tello_driver_main', output='screen', namespace=namespace, ),
+
+        Node(package='tello_position', executable='tello_position_cal_CATS', output='screen', namespace=namespace),
+
+        # Spawn tello_1.urdf
+        Node(package='gazebo_ros', executable='spawn_entity.py', output='screen', namespace=namespace,
+             arguments=[
+                '-file', urdf_path,
+                '-entity', namespace,
+                '-robot_namespace', namespace
+            ]
+        ),
+    ])
